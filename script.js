1fa4d0ce42271bb32a7411182d93c7ea3f329653
miguelMedinaCastro <miguelmedinadecastro@gmail.com>
2025-10-19 15:52:11 -0300

    parcialmente feito

--- script.js
+++ script.js
@@ -2,13 +2,31 @@
 const dropdownContainer = document.getElementById('dropdown-container');
 const dropdownButton = document.getElementById('dropdown-button');
 const dropdownMenu = document.getElementById('dropdown-menu');
+const patife = document.getElementById('patife');
+const patife2 = document.getElementById('patife2');
+const botao1 = document.getElementById('botao1')
+const botao2 = document.getElementById('botao2')
+
 
 dropdownButton.addEventListener('click', (event) => {
     event.stopPropagation(); 
     dropdownMenu.classList.toggle('hidden');
 });
-    document.addEventListener('click', (event) => {
+
+document.addEventListener('click', (event) => {
     if (!dropdownContainer.contains(event.target) && !dropdownMenu.classList.contains('hidden')) {
         dropdownMenu.classList.add('hidden');
     }
+});
+
+botao1.addEventListener('click', (event) => {
+    event.stopPropagation(); 
+    patife.classList.toggle('hidden');
+    patife2.classList.add('hidden');
+});
+
+botao2.addEventListener('click', (event) => {
+    event.stopPropagation(); 
+    patife2.classList.toggle('hidden');
+    patife.classList.add('hidden');
 });
