0e322dd663f3ee809fc64718c7039e8793c4cffe
miguelMedinaCastro <miguelmedinadecastro@gmail.com>
2025-10-22 14:50:52 -0300

    blocos organizados

--- src/script.js
+++ src/script.js
@@ -2,11 +2,12 @@
 const dropdownContainer = document.getElementById('dropdown-container');
 const dropdownButton = document.getElementById('dropdown-button');
 const dropdownMenu = document.getElementById('dropdown-menu');
-const patife = document.getElementById('patife');
-const patife2 = document.getElementById('patife2');
-const botao1 = document.getElementById('botao1')
-const botao2 = document.getElementById('botao2')
-
+const miguel = document.getElementById('miguel');
+const milena = document.getElementById('milena');
+const bruno = document.getElementById('bruno');
+const botao1 = document.getElementById('botao1');
+const botao2 = document.getElementById('botao2');
+const botao3 = document.getElementById('botao3');
 
 dropdownButton.addEventListener('click', (event) => {
     event.stopPropagation(); 
@@ -21,12 +22,21 @@ document.addEventListener('click', (event) => {
 
 botao1.addEventListener('click', (event) => {
     event.stopPropagation(); 
-    patife.classList.toggle('hidden');
-    patife2.classList.add('hidden');
+    miguel.classList.toggle('hidden');
+    bruno.classList.add('hidden');
+    milena.classList.add('hidden');
 });
 
 botao2.addEventListener('click', (event) => {
     event.stopPropagation(); 
-    patife2.classList.toggle('hidden');
-    patife.classList.add('hidden');
+    milena.classList.toggle('hidden');
+    bruno.classList.add('hidden');
+    miguel.classList.add('hidden');
+});
+
+botao3.addEventListener('click', (event) => {
+    event.stopPropagation(); 
+    bruno.classList.toggle('hidden');
+    milena.classList.add('hidden');
+    miguel.classList.add('hidden');
 });
